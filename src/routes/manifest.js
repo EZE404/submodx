570076056e16984d5d69dc340f989547e480d16f
Franco Ezequiel <franco.ezequiel@outlook.com>
2026-05-29 02:07:31 -0300

    Validating token during manifest request

--- src/routes/manifest.js
+++ src/routes/manifest.js
@@ -1,4 +1,5 @@
 const { getManifest } = require('../manifest')
+const { decryptApiKey } = require('../crypto')
 const config = require('../config')
 
 function manifestRoute(req, res) {
@@ -6,6 +7,14 @@ function manifestRoute(req, res) {
   console.log(`[SubX] manifestRoute: config=${req.params.config}, configurationRequired=${configurationRequired}`)
   res.setHeader('Content-Type', 'application/json; charset=utf-8')
   res.setHeader('Access-Control-Allow-Origin', '*')
+
+  if (!configurationRequired) {
+    const apiKey = decryptApiKey(req.params.config)
+    if (!apiKey) {
+      console.log(`[SubX] manifestRoute: invalid config token, returning 400`)
+      return res.status(400).json({ error: 'Token de configuración inválido' })
+    }
+  }
 
   const manifest = getManifest(config.baseUrl, { configurationRequired })
   res.json(manifest)
